db8f478d8794535e08a115c7f7c020bb8ea1c4ab
Chris Hamm <chris.m.hamm@gmail.com>
2015-06-10 16:56:31 -0400

    all working but print to html

---
+++ app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    var fizzBuzz = [];
+    var fillArray = 1;
+    var fizzIndex = 0;
+
+
+    while(fillArray <= 100) {
+        if (fillArray % 3 == 0 && fillArray % 5 ==0) {
+            fizzBuzz.push('FizzBuzz');
+        }
+        else if (fillArray % 5 == 0) {
+            fizzBuzz.push('Buzz');
+        }
+        else if (fillArray % 3 == 0) {
+            fizzBuzz.push('Fizz');
+        }
+        else (fizzBuzz.push(fillArray));
+
+        $('.printFB:first-child').append(fizzBuzz[fizzIndex]);
+        fillArray++;
+        fizzIndex++;
+    };
+
+
+});

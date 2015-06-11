560da70aae4b615a144f57c39949b983bb585a2d
Chris Hamm <chris.m.hamm@gmail.com>
2015-06-11 17:42:44 -0400

    final final

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    var fizzBuzz = [];
+/*    var fizzBuzz = [];
     var fillArray = 1;
     var fizzIndex = 0;
 
@@ -22,5 +22,40 @@ $(document).ready(function(){
         fizzIndex++;
     };
 
+
+*/
+
+function fizzbuzz(max) {
+    for ( var i = 1; i <= max; i++ ) {
+        if (i % 5 == 0 && i % 3 == 0 ) {
+            $('.printFB').append('<li>' + "FizzBuzz" + '</li>');
+        } else if (i % 3 == 0) {
+            $('.printFB').append('<li>' + "Fizz" + '</li>');
+        } else if (i % 5 == 0) {
+            $('.printFB').append('<li>' + "Buzz" + '</li>');
+        } else {
+            $('.printFB').append('<li>' + i + '</li>');
+        }
+    }
+}
+
+//Below is not working and is breaking FizzBuzz. Likely incorrect usage of parseInt
+$('.input-number').click(function() {
+    var parseTemp = $('input').val();
+    if (isNaN(parseInt(parseTemp))) {
+        alert("Hey, that's not a number, dummy!");
+    }
+    else if ($('input').val() % 1 != 0) {
+        alert("Please use only whole numbers");
+    }
+    else {
+        fizzbuzz(parseInt(parseTemp));
+    }
+    return $('input').val("");
+});
+
+$(document).on('click', '.reset', function() {
+    $('.printFB').children().remove();
+});
 
 });
